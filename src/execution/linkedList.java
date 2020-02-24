258d8297a8a12846199e6697ef9cb55487c046d3
alpha <vijaytyagi369>
2020-02-24 22:41:39 +0530

    Getters Created

--- src/execution/linkedList.java
+++ src/execution/linkedList.java
@@ -30,5 +30,18 @@ public class linkedList<E>implements singleLinkedListadt<E> {
             this.data=data;
             this.next=next;
         }
+
+        private Node(E data){
+            this.data=data;
+        }
+        private Node(){}
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext(){
+            return next;
+        }
     }
 }
