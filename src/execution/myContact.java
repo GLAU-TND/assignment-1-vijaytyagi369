a4069dcbb9fa4694954faa295084af6441571db4
alpha <vijaytyagi369>
2020-02-24 23:09:49 +0530

    Method To get First Name

---
+++ src/execution/myContact.java
@@ -0,0 +1,43 @@
+package execution;
+
+import contactADT.contactADT;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class myContact implements contactADT {
+    Scanner sc = new Scanner(System.in);
+
+    linkedList<person> MyContactsBook = new linkedList<>();
+    linkedList<String> contactNumbers = new linkedList<>();
+
+
+    @Override
+    public void addContact() {
+
+    }
+
+    @Override
+    public void viewContacts() {
+
+    }
+
+    @Override
+    public void deleteContact() {
+
+    }
+
+    @Override
+    public void searchContact() {
+
+    }
+
+    private String GetFirstName() {
+        System.out.println("Please Enter the name of the person");
+        System.out.print("FirstName: ");
+        String firstname = sc.next();
+        return firstname;
+
+    }
+}
