49d4580becfdee82158d1201f488c15c99efd043
alpha <vijaytyagi369>
2020-02-24 22:58:52 +0530

    Person Class Is Created

---
+++ src/execution/person.java
@@ -0,0 +1,19 @@
+package execution;
+
+
+public class person {
+
+    private String firstName;
+    private String lastName;
+    private linkedList<String> contactNumbers;
+    private String email;
+
+    public person(String firstName, String lastName, linkedList<String> contactNumbers, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNumbers = contactNumbers;
+        this.email = email;
+    }
+
+
+}
