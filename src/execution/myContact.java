10c6cc2fb816c923dbfa1d6afb9bbd360cc29c42
alpha <vijaytyagi369>
2020-02-24 23:11:43 +0530

    Method To get Email

--- src/execution/myContact.java
+++ src/execution/myContact.java
@@ -89,4 +89,28 @@ public class myContact implements contactADT {
 
 
     }
+
+    private String GetEmail() {
+        String Email = null;
+        while (true) {
+
+            System.out.print("Do You Want to add an email ? (y/n) :");
+            String a = ((sc.next()).toLowerCase());
+            char at = a.charAt(0);
+            if (a.length() > 1) {
+                System.out.println("Please Enter a Vaild Input i.e., y(lowercase) for Yes or n(lowercase for NO)");
+                continue;
+            }
+            if (at == 'y') {
+                System.out.print("Email Address: ");
+                Email = sc.next();
+            } else if (at == 'n') {
+                break;
+            } else {
+                System.out.println("Please Enter a Vaild Input i.e., y(lowercase) for Yes or n(lowercase for NO)");
+            }
+        }
+        return Email;
+    }
+
 }
